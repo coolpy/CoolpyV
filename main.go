5cff268de871e6bc1fcaa835839695b24d4e3aea
JacobLai <jacle169@21cn.com>
2016-05-18 17:43:36 +0800

    小修正

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var Version = "5.0.0.0"
 
 func main() {
 	fmt.Printf("Coolpy Version: %s\n", Version)
-        if _, err := os.Stat("conf.json"); err != nil {
+	if _, err := os.Stat("conf.json"); err != nil {
 		log.Fatal("Config file is missing: ", "conf.json")
 		os.Exit(1)
 	}
@@ -31,28 +31,28 @@ func main() {
 		os.Exit(1)
 	}
 	conf, ok := configuration.(map[string]interface{})
-        if ok{
-		for k,v := range conf{
+	if ok {
+		for k, v := range conf {
 			switch v2 := v.(type) {
 			case string:
 				if k == "mongo" {
 					Mdb.DatabaseAddress = v2
 				}
 			default:
-				fmt.Println(k,v)
+				fmt.Println(k, v)
 			}
 		}
 	}
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.POST("/",IndexPost)
+	router.POST("/", IndexPost)
 	router.GET("/hello/:name", Basicauth.Auth(Hello))
-        if err := http.ListenAndServe(":8080", Cors.CORS(router)); err != nil{
+	if err := http.ListenAndServe(":8080", Cors.CORS(router)); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func IndexPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+func IndexPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	decoder := json.NewDecoder(r.Body)
 	var p Models.Person
 	err := decoder.Decode(&p)
@@ -61,7 +61,7 @@ func IndexPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
 		log.Fatal(err)
 		return
 	}
-	Mdb.M("people", func(c * mgo.Collection) {
+	Mdb.M("people", func(c *mgo.Collection) {
 		err = c.Insert(p)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
@@ -72,17 +72,17 @@ func IndexPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
 	})
 }
 
-  func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	  Mdb.M("people", func(c *mgo.Collection) {
-		  result := []Models.Person{}
-		  err := c.Find(bson.M{"name": "Ale"}).All(&result)
-		  if err != nil {
-			  log.Fatal(err)
-		  }
-		  json.NewEncoder(w).Encode(result)
-	  })
-  }
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	Mdb.M("people", func(c *mgo.Collection) {
+		result := []Models.Person{}
+		err := c.Find(bson.M{"name": "Ale"}).All(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.NewEncoder(w).Encode(result)
+	})
+}
 
-  func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-      fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-  }
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
