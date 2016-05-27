38634a1d61abc0581a3f3794eadf274446e9c63a
biless <biless@qq.com>
2016-05-27 21:56:51 +0800

    添加速度测试

--- lib/src/mqtt/mqtt_test.go
+++ lib/src/mqtt/mqtt_test.go
@@ -1,7 +1,34 @@
 package mqtt
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"time"
+)
 
+const size = 10000000
+
+func TestGetBytesPush(t *testing.T) {
+	t0 := time.Now()
+	for i := 1; i < size; i++ {
+		_ = GetBytes(128)
+	}
+	t1 := time.Now()
+	fmt.Printf("The testIntVectorPush call took %v to run.\n", t1.Sub(t0))
+}
+
+func TestGetHeaderPush(t *testing.T) {
+	t0 := time.Now()
+	for i := 1; i < size; i++ {
+		header := GetHeader(0x1B)
+		if(header.Control != Connect) {
+
+		}
+	}
+	t1 := time.Now()
+	fmt.Printf("The TestGetHeader call took %v to run.\n", t1.Sub(t0))
+}
+3
 func TestGetHeader(t *testing.T) {
 	header := GetHeader(0x1B)
 	if(header.Control != Connect) {
